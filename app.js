5bc440f561a0a7b524f04881db2176a4b2c0bd7d
Taylan Güloğlu <guloglutaylan@gmail.com>
2022-04-12 15:36:07 +0300

    Last Version

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+const methodOverride = require('method-override');
+//const bcrypt = require('bcrypt')
 
 const pageRoute = require('./routes/pageRoute');
-const courseRoute = require('./routes/courseRoute')
-const categoryRoute = require('./routes/categoryRoute')
+const courseRoute = require('./routes/courseRoute');
+const categoryRoute = require('./routes/categoryRoute');
+const userRoute = require('./routes/userRoute');
 
 const app = express();
 
@@ -22,16 +28,41 @@ mongoose
 //Template Engine
 app.set('view engine', 'ejs');
 
+//Global Variable
+global.userIn = null;
+
 //Middlewares
 app.use(express.static('public'));
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(
+  session({
+    secret: 'my_keyboard_cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: 'mongodb://localhost/smartedu-db' }),
+  })
+);
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.flashMessages = req.flash();
+  next();
+});
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
 //Routes
+app.use('*', (req, res, next) => {
+  userIn = req.session.userID;
+  next();
+});
 app.use('/', pageRoute);
 app.use('/courses', courseRoute);
 app.use('/categories', categoryRoute);
-
+app.use('/users', userRoute);
 
 const port = 3000;
 app.listen(port, () => {
